e6e4d10feb1e5c47462bb77515eac2e231c56e71
Yogesh054 <99482190+Yogesh054@users.noreply.github.com>
2022-03-24 18:43:04 +0530

    'added'

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+function Card()
+{
+return(
+    <div className="ID">
+        <h3 className="title">TOP NOTCH</h3>
+        <span className="text"><span>Name:</span>Yogesh</span>
+        <br></br>
+        <span className="text"><span>Age:</span>19</span>
+    </div>
+)
+};
+export default Card;
